7f596dc2f39bf44fc39f418bd25dcf51774d43c7
Jeremy Lai <jeremylai0812@gmail.com>
2019-10-09 17:44:07 +0800

    UI improval

--- maskbot/forms.py
+++ maskbot/forms.py
@@ -34,11 +34,11 @@ class UploadimgForm(forms.Form):
 class MattingForm(forms.Form):
     original = forms.IntegerField(required=True, widget=forms.NumberInput(attrs={'type':'hidden'}))
     mask = forms.IntegerField(required=True, widget=forms.NumberInput(attrs={'type':'hidden'}))
-    size = forms.IntegerField(required=True, label="外擴參數", label_suffix=":", initial=0, min_value=0, max_value=20,
+    size = forms.IntegerField(required=True, label="調整邊緣(細-寬)", label_suffix=":", initial=0, min_value=0, max_value=20,
                                 widget=forms.NumberInput(attrs={'type':'range','step':1,'onchange':'updateText(this.value);'}))
     #temp is only for display of size
     temp = forms.IntegerField(label="", label_suffix="", initial=0,
-                                widget=forms.NumberInput(attrs={'readonly':True}))
+                                widget=forms.NumberInput(attrs={'readonly':True,'type':'hidden'}))
 
     def clean_original(self):
         data = self.cleaned_data['original']
